be5e4d21a7d2756412d5310a3c541fe1bb8f160f
Joseph Zhong <joseph.zhong@hotmail.com>
2017-03-18 04:17:15 -0700

    complete update.sh

--- update.sh
+++ update.sh
@@ -2,15 +2,34 @@
 #
 # update.sh
 # 
-# Script to automatically set symblinks for each dotfile found 
+#   Script to automatically set symblinks for each dotfile
+# 
+# Notes:
+#   We ignore ., .., .DS_Store .gitignore  and .git -- in the future a list of 
+#   files to ignore should be listed in a file. If the symblink already exists, 
+#   we will forcefully overwrite the previous symblink and source the dotfile if
+#   the dotfile contains bash in the filename
+#
 
+DOTFILES_DIR=`pwd`
+DOTFILES=$DOTFILES_DIR/.*
 
-DOTFILES=~/.dotfiles
-FILES="$DOTFILES"/.*
-
-for dotfile in "$FILES"
+for dotfile in $DOTFILES
 do
-    if [ "$dotfile" != $DOTFILES"/." -a "$dotfile" != $DOTFILES"/.." ]; then
-        echo $dotfile
-    fi 
+    if [ $DOTFILES_DIR != $dotfile ] \
+        && [ $DOTFILES_DIR'/.' != "$dotfile" ] \
+        && [ $DOTFILES_DIR'/..' != "$dotfile" ] \
+        && [ $DOTFILES_DIR'/.git' != "$dotfile" ] \
+        && [ $DOTFILES_DIR'/.DS_Store' != "$dotfile" ]
+    then
+        printf $'creating symblink for '$dotfile$'\n'
+        ln -sf $dotfile ~/$(basename $dotfile) 
+        if [ 'bash' == *$dotfile* ]
+        then
+            printf 'sourcing '~/$(basename $dotfile)$'\n'
+            source ~/$(basename $dotfile)
+        fi
+    else
+        printf $'ignoring '$dotfile$'\n'
+    fi
 done
