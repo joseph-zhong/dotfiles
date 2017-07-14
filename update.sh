d61d17b8f1d060f6221210e812284812303e080b
Joseph Zhong <joseph.zhong@hotmail.com>
2017-07-14 16:21:03 -0700

    remove vim_runtime and symb link my configs

--- update.sh
+++ update.sh
@@ -24,14 +24,14 @@ do
     then
         printf $'creating symblink for '$dotfile$'\n'
         ln -sf $dotfile ~/$(basename $dotfile)
-        if [ 'bash' == *$dotfile* ]
-        then
-            printf 'sourcing '~/$(basename $dotfile)$'\n'
-            source ~/$(basename $dotfile)
-        fi
     else
         printf $'ignoring '$dotfile$'\n'
     fi
 done
 
+git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+printf 'creating symblink for my_configs.vim\n'
+ln -sf my_configs.vim ~/.vim_runtime/
+
