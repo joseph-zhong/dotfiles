927e6cdbdba7e1b92d2722bafef7b256b772f859
Joseph Zhong <josephz@cs.washington.edu>
2020-11-20 11:09:05 -0500

    windows is horrible

--- update.ps1
+++ update.ps1
@@ -1,20 +1,21 @@
 # 
-# update.ps3
+# update.ps1
 #
 # 	Script to automatically set symbllinks for each dotfile (Windows)
 #
+# Run powershell_configs/make_symblink.ps1 first.
 
 # vimrc.
-git clone https://github.com/joseph-zhong/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
+git clone https://github.com/joseph-zhong/vimrc.git ../.vim_runtime
+powershell ../.vim_runtime/install_awesome_vimrc.ps1
 
-printf 'creating symblink for my_configs.vim\n'
-ln -sf ~/dotfiles/my_configs.vim ~/.vim_runtime/
+write-output "creating symblink for my_configs.vim\n"
+symblink ~/dotfiles/my_configs.vim ~/.vim_runtime/
 
-printf 'installing fzf...\n'
-cd .fzf && ./install
+# printf 'installing fzf...\n'
+# .fzf/install
 
-echo
-echo "Done updating dotfiles... Please run \`source .bashrc\`"
-echo
+# echo
+# echo "Done updating dotfiles... Please run \`source .bashrc\`"
+# echo
 
