8457f7a1a3742053880ef22ad824deed58f0d260
Joseph Zhong <josephz@cs.washington.edu>
2021-06-07 11:39:28 -0400

    update win instructions

--- powershell_configs/profile.ps1
+++ powershell_configs/profile.ps1
@@ -27,8 +27,10 @@ Function symblink($src, $dst) {
     New-Item -ItemType symboliclink -Path $dst -Target $src
 }
 
+
+
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
-(& "C:\Users\josephz\Miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+(& "C:\Users\josephz\AppData\Local\Continuum\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
 #endregion
 
